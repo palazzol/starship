1810022263767c79c7205c2412e6fe02e9b9cab9
Frank Palazzolo <frank@avoidspikes.com>
2015-02-11 11:43:28 -0500

    Old python version

---
+++ python/sr.py
@@ -0,0 +1,183 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 24 13:44:06 2011
+
+@author: palazzol
+"""
+
+from numpy import *
+import pylab as plt
+
+def Metric(a,b):
+    'Minkowski Metric'
+    return -(a[0]*b[0])+a[1]*b[1]+a[2]*b[2]+a[3]*b[3]
+            
+def ProjectTime(pos, vel):
+    'Solve the equation (pos-x*vel)^(pos-x*vel) == 0.0 for x>=0'
+    A = Metric(vel,vel)
+    B = Metric(pos,vel) * -2
+    C = Metric(pos,pos)
+    D=B*B-4*A*C
+    if math.fabs(D)<1e-6:
+        D = 0.0
+    x1 = (-B+math.sqrt(D))/(2*A)
+    x2 = (-B-math.sqrt(D))/(2*A)
+    if x1<x2:
+        x=x1
+    else:
+        x=x2
+    #print x,x1,x2
+    if math.fabs(x)<1e-6:
+        x = 0.0
+    return x
+    
+class Universe():
+    def __init__(self):
+        self.__c = 100.0
+        self.__observer = None
+        self.__objects = []
+    def AddObject(self, obj):
+        self.__objects.append(obj)
+    def GetObjects(self):
+        return self.__objects
+    def GetPos3(self, obj):
+        global_delta = obj.GetGlobalPos4()-self.__observer.GetGlobalPos4()
+        local_delta = dot( global_delta, self.__observer.GetLorentz() )
+        #new_pos = self.__observer.GetGlobalPos4()+local_delta
+        new_pos = local_delta
+        return new_pos[1:4]
+    def GetSpeedOfLight(self):
+        return self.__c
+    def SetSpeedOfLight(self, c):
+        self.__c = c
+    def SetObserver(self, obj):
+        self.__observer = obj
+    def GetObserver(self):
+        return self.__observer
+    def MoveToObserverClock(self, tau):
+        'Actually move the observer to the event where this proper time is measured'
+        delta_tau = tau-self.__observer.GetClock()
+        self.__observer.IncrementTime(delta_tau)
+        #print self.__observer.GetName(),delta_tau        
+        for obj in self.__objects:
+            if obj != self.__observer:
+                'Move the object to the event where observer will "see" it'
+                delta_tau = ProjectTime(self.__observer.GetGlobalPos4()-obj.GetGlobalPos4(), obj.GetGlobalVel4())
+                #if obj.GetName() != 'beacon':
+                #    print obj.GetName(),delta_tau        
+                obj.IncrementTime(delta_tau)
+    
+class Object():
+    'name, pos3, vel3, clock, restmass'
+    def __init__(self, universe, name, pos3, vel3, clock, restmass):
+        self.__universe = universe
+        self.__c = self.__universe.GetSpeedOfLight()
+        # Initialize the name
+        self.__name = name
+        # Initialize the position 4-vector
+        self.__pos4 = array([self.__c*clock,pos3[0],pos3[1],pos3[2]])
+        # Initialize the velocity 4-vector
+        self.SetGlobalVel3(vel3)
+        # Initialize the clock
+        self.__clock = clock 
+        # Initialize the orientation
+        self.__orient3 = array([1.0,0.0,0.0])
+        # Initialize the rest mass
+        self.__restmass = restmass
+        #self.__accel4 = array([0.0,0.0,0.0,0.0])
+        self.__force = array([0.0,0.0,0.0,0.0])
+        self.__universe.AddObject(self)
+    def GetName(self):
+        return self.__name
+    def GetClock(self):
+        return self.__clock
+    def SetClock(self,clock):
+        self.__clock = clock
+    def MoveToClock(self,clock):
+        delta_tau = clock-self.__clock
+        self.IncrementTime(delta_tau)
+    def GetOrientation(self):
+        return self.__orient3
+    def Turn(self, degrees):
+        radians = degrees*math.pi/180.0
+        o_old = self.GetOrientation()
+        o_new = array([0.0,0.0])
+        o_new[0] = o_old[0]*math.cos(radians)+o_old[1]*math.sin(radians)
+        o_new[1] = -o_old[0]*math.sin(radians)+o_old[1]*math.cos(radians)
+        self.__orient3 = array([o_new[0],o_new[1],self.__orient3[2]])
+    def GetGlobalPos3(self):
+        return self.__pos4[1:4]  
+    def GetGlobalPos4(self):
+        return self.__pos4
+    def GetGlobalVel4(self):
+        return self.__vel4
+    def SetGlobalVel3(self, vel3):
+        #v_squared = vel3[0]*vel3[0]+vel3[1]*vel3[1]+vel3[2]*vel3[2]
+        c = self.__c
+        bx = vel3[0]/c
+        by = vel3[1]/c
+        bz = vel3[2]/c
+        b_squared = bx*bx+by*by+bz*bz
+        gamma = 1.0/math.sqrt(1-b_squared)
+        self.SetGlobalVel4(array([c,vel3[0],vel3[1],vel3[2]])*gamma)
+    def SetGlobalVel4(self, vel4):
+        self.__vel4 = vel4
+        gamma = self.__vel4[0]/self.__c
+        bx = self.__vel4[1]/self.__c/gamma
+        by = self.__vel4[2]/self.__c/gamma
+        bz = self.__vel4[3]/self.__c/gamma
+        b_squared = bx*bx+by*by+bz*bz
+        self.__lorentz = zeros((4,4))
+        if b_squared < 1e-6:
+            self.__lorentz[0][0] = 1.0
+            self.__lorentz[1][1] = 1.0
+            self.__lorentz[2][2] = 1.0
+            self.__lorentz[3][3] = 1.0
+        else:            
+            self.__lorentz[0][0] = gamma
+            self.__lorentz[0][1] = -bx*gamma
+            self.__lorentz[0][2] = -by*gamma
+            self.__lorentz[0][3] = -bz*gamma
+            self.__lorentz[1][0] = self.__lorentz[0][1]             
+            self.__lorentz[1][1] = 1.0+(gamma-1)*bx*bx/b_squared
+            self.__lorentz[1][2] = (gamma-1)*bx*by/b_squared
+            self.__lorentz[1][3] = (gamma-1)*bx*bz/b_squared
+            self.__lorentz[2][0] = self.__lorentz[0][2]             
+            self.__lorentz[2][1] = self.__lorentz[1][2]
+            self.__lorentz[2][2] = 1.0+(gamma-1)*by*by/b_squared
+            self.__lorentz[2][3] = (gamma-1)*by*bz/b_squared
+            self.__lorentz[3][0] = self.__lorentz[0][3]             
+            self.__lorentz[3][1] = self.__lorentz[1][3]
+            self.__lorentz[3][2] = self.__lorentz[2][3]
+            self.__lorentz[3][3] = 1.0+(gamma-1)*bz*bz/b_squared
+    def GetLorentz(self):
+        return self.__lorentz
+    def GetGlobalTime(self):
+        return self.__pos4[0]/self.__c
+    def ApplyForce(self, force3):
+        self.__force = array([0,force3[0],force3[1],force3[2]])
+        #vp = math.sqrt(dot(force3, force3))
+        #bp = vp/self.__c
+        #gp = 1.0/math.sqrt(1-bp*bp)
+        #self.__dv = gp*array([self.__c,force3[0],force3[1],force3[2]])
+        #self.__accel4 = self.__gamma/self.__restmass*array([sum(force3*self.__vel4[1:4])/self.__c,force3[0],force3[1],force3[2]])
+    def IncrementTime(self,delta_tau):
+        new_vel = self.__vel4 + self.__force*delta_tau/self.__restmass
+        D_squared = dot( new_vel[1:4], new_vel[1:4] )
+        gamma = 1.0/math.sqrt(1.0-D_squared/(self.__c*self.__c+D_squared))
+        new_vel[0] = gamma*self.__c
+        self.SetGlobalVel4(new_vel)
+        #if self.__name == "ship":
+            #print 'D_2=',D_squared
+            #print 'b=',math.sqrt(D_squared/(self.__c*self.__c+D_squared))
+            #print Metric(self.__vel4, self.__vel4), self.__vel4
+            #print 'v_x =',self.__vel4[1]/gamma
+            #print
+        #self.__vel4 = p_after/self.__restmass
+        #self.__gamma = self.__vel4[0]/self.__c
+        #self.__vel4 = self.__vel4 + self.__accel4*delta_tau
+        self.__pos4 = self.__pos4 + self.__vel4*delta_tau
+        self.__clock += delta_tau
+        #if self.GetName() != 'beacon':
+        #    print self.__clock,delta_tau        
+
---
+++ python/starship2.py
@@ -0,0 +1,265 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 03 10:32:13 2011
+
+@author: Frank
+"""
+
+#Import Modules
+from numpy import *
+import os, pygame
+from pygame.locals import *
+import sr
+
+import math
+
+if not pygame.font: print 'Warning, fonts disabled'
+if not pygame.mixer: print 'Warning, sound disabled'
+
+pygame.font.init()
+font = pygame.font.Font(None,16)
+
+class Object2D(sr.Object):
+    def __init__(self, universe, name, pos3, vel3, clock, restmass):
+        sr.Object.__init__(self, universe, name, pos3, vel3, clock, restmass)
+        self.__color = (255,255,255)
+        self.__radius = 10
+    def SetColor(self, color):
+        self.__color = color
+    def SetRadius(self, radius):
+        self.__radius = radius
+    def Draw(self, surf, center):
+        pos = self._Object__universe.GetPos3(self)
+        pos = (int(pos[0]+0.5)+center[0], int(pos[1]+0.5)+center[1])
+        if self.__radius == 1:
+            l = 3
+            pygame.draw.line(surf, self.__color, (pos[0]-l,pos[1]), (pos[0]+l,pos[1]), 1) 
+            pygame.draw.line(surf, self.__color, (pos[0],pos[1]-l), (pos[0],pos[1]+l), 1) 
+            #pygame.draw.circle(surf, self.__color, pos, self.__radius, 1)
+        else:
+            pygame.draw.circle(surf, self.__color, pos, self.__radius, 0) 
+        if self.GetName() != 'beacon':
+            t = self.GetClock()
+            txt = font.render("Clock=%s.%s" % (int(t),int(t*10)%10), True, self.__color)
+            surf.unlock()
+            surf.blit(txt, (pos[0]+self.__radius, pos[1]+self.__radius))
+            surf.lock()
+        
+class Ship2D(sr.Object):
+    def __init__(self, universe, name, pos3, vel3, clock, restmass):
+        sr.Object.__init__(self, universe, name, pos3, vel3, clock, restmass)
+        self.__color = (255,255,255)
+        self.__wire = [(1.0,0.0),(-1.0,-0.7),(-0.5,0.0),(-1.0,0.7)]
+        #self.__wire = [(-0.5,0.0),(0.5,-0.3),(-0.5,-0.6),
+        #               (-0.9,-0.6),(-0.8,-0.5),(-0.3,-0.4),
+        #               (-0.6,-0.3),(-0.3,-0.2),(-0.8,-0.1),
+        #               (-0.8,0.1),(-0.3,0.2),(-0.6,0.3),
+        #               (-0.3,0.4),(-0.8,0.5),(-0.9,0.6),
+        #               (-0.5,0.6),(0.5,0.3)]
+        self.__flame = [(-0.75,-0.35),(-1.5,0.0),(-0.75,0.35)]
+        self.__toggle = False
+        self.__thruster = False
+        self.SetRadius(10)
+    def SetColor(self, color):
+        self.__color = color
+    def SetRadius(self, radius):
+        self.__radius = radius
+    def Thrust(self, amount):
+        if amount > 0:
+            self.__thruster = True
+        else:
+            self.__thruster = False
+        self.ApplyForce(self.GetOrientation()*amount)
+    def Draw(self, surf, center):
+        pos = self._Object__universe.GetPos3(self)
+        pos = (int(pos[0]+0.5)+center[0], int(pos[1]+0.5)+center[1])
+        vel4 = self.GetGlobalVel4()
+        c = self._Object__c
+        g = vel4[0]/self._Object__c
+        gv = math.sqrt(dot(vel4[1:4],vel4[1:4]))
+        v = gv/g
+        t = self.GetClock()
+        for r in range(50,600,50):
+            center_offset = r
+            center_pos = (int(pos[0]+vel4[1]/c*center_offset),int(pos[1]+vel4[2]/c*center_offset))
+            radius = int(r*g)
+            #center_pos = (int(pos[0]+xaxis[0]),int(pos[1]+xaxis[1]))
+            pygame.draw.circle(surf, (64,64,64), center_pos, radius, 1)
+        if self.__radius == 1:
+            pygame.draw.circle(surf, self.__color, pos, self.__radius, 1)
+        else:
+            xaxis = self.GetOrientation()[0:2]
+            yaxis = array([-xaxis[1],xaxis[0]])
+            p_rots = []
+            for p in self.__wire:
+                p_rot = (p[0]*xaxis + p[1]*yaxis)*self.__radius
+                p_rot[0] += pos[0]
+                p_rot[1] += pos[1]
+                p_rots.append(p_rot)
+            pygame.draw.lines(surf, self.__color, True, p_rots, 1)
+            if self.__toggle:
+                self.__toggle = False
+            else:
+                self.__toggle = True
+            if (self.__thruster and self.__toggle):
+                p_rots = []
+                for p in self.__flame:
+                    p_rot = (p[0]*xaxis + p[1]*yaxis)*self.__radius
+                    p_rot[0] += pos[0]
+                    p_rot[1] += pos[1]
+                    p_rots.append(p_rot)
+                pygame.draw.lines(surf, self.__color, False, p_rots, 1)
+        txt = font.render("v/c=%s%%, Clock=%s.%s" % (int(v/c*100),int(t),int(t*10)%10), True, self.__color)
+        surf.unlock()
+        surf.blit(txt, (pos[0]+self.__radius, pos[1]+self.__radius))
+        surf.lock()
+
+            
+def main():
+    """this function is called when the program starts.
+       it initializes everything it needs, then runs in
+       a loop until the function returns."""
+#Initialize Everything
+    pygame.init()
+    size = (1024,768) #ipad
+    #size = (640, 480)
+    screen = pygame.display.set_mode(size)
+    center = (size[0]/2,size[1]/2)
+    pygame.display.set_caption('Starship 2')
+    pygame.mouse.set_visible(0)
+
+#Create The Backgound
+    background = pygame.Surface(screen.get_size())
+    background = background.convert()
+    background.fill((0, 0, 0))
+
+#Prepare Game Objects
+    clock = pygame.time.Clock()
+    u = sr.Universe()
+    objects = []
+    #for i in range(-14,15):
+    #    for j in range(-14,14):
+    for i in range(-10,11):
+        for j in range(-10,11):
+            if i==0 and j==0:
+                continue
+            beacon = Object2D(u, "beacon", (i*50,j*50,0), (0,0,0), 0, 1)
+            #beacon = Object2D(u, "beacon", (math.cos(math.pi*i/7.5)*(j+15)*20,math.sin(math.pi*i/7.5)*(j+15)*20,0), (0,0,0), 0, 1)
+            beacon.SetColor((128,128,128))
+            beacon.SetRadius(1)
+            objects.append(beacon)
+    
+    ship2   = Object2D(u, "ship2",   (-200,50,0), (0,0,0), 0, 10)
+    ship2.SetColor((0,0,255))
+    ship2.SetRadius(5)
+    objects.append(ship2)
+    ship3   = Object2D(u, "ship3",   (-200,150,0), (0,0,0), 0, 10)
+    ship3.SetColor((255,0,0))
+    ship3.SetRadius(5)
+    objects.append(ship3)
+
+    station = Object2D(u, "station",(0,0,0), (0,0,0), 0, 100)
+    station.SetColor((255,255,0))
+    station.SetRadius(10)
+    objects.append(station)
+    ship    = Ship2D(u, "ship",   (-200,50,0), (0,0,0), 0, 10)
+    ship.SetColor((0,255,0))
+    ship.SetRadius(20)
+    objects.append(ship)
+
+    u.SetObserver(ship)
+    speed = 50
+
+    surf = pygame.display.get_surface()
+    tau = 0
+    delta_tau = 0.04167
+    
+    delta_turn = 10.0 #degrees
+    
+    turn = 0.0
+    thrust = 200.0
+    
+#Main Loop
+    while True:
+        clock.tick(int(1.0/delta_tau))
+
+    #Handle Input Events
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                return
+            """
+            elif event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    pygame.quit()
+                    return
+                elif event.key == K_z:
+                    turn = +delta_turn
+                    ship.Turn(turn)
+                elif event.key == K_x:
+                    turn = -delta_turn
+                    ship.Turn(turn)
+            elif event.type == KEYUP:
+                if event.key == K_z:
+                    turn = 0.0
+                    ship.Turn(turn)
+                elif event.key == K_x:
+                    turn = 0.0
+                    ship.Turn(turn)
+            elif event.type == MOUSEBUTTONDOWN:
+                #ship.SetGlobalVel3((speed,0,0))
+                #ship2.SetGlobalVel3((speed,0,0))
+                #ship3.SetGlobalVel3((speed,0,0))
+                if event.button == 1:
+                    ship.ApplyForce(10*ship.GetOrientation())
+                    #ship2.ApplyForce((10.0,0,0))
+                    #ship3.ApplyForce((10.0,0,0))
+                    if delta_tau == 0.0:
+                        delta_tau = 1.0
+                #else:
+                    #ship.ApplyForce((-10.0,0,0))
+                    #ship2.ApplyForce((-10.0,0,0))
+                    #ship3.ApplyForce((-10.0,0,0))
+            elif event.type is MOUSEBUTTONUP:
+                ship.ApplyForce((0.0,0,0))
+                ship2.ApplyForce((0.0,0,0))
+                ship3.ApplyForce((0.0,0,0))
+            """    
+        keys = pygame.key.get_pressed()
+        if keys[K_ESCAPE]:
+            pygame.quit()
+            return
+        if keys[K_z]:
+            turn = +delta_turn
+            ship.Turn(turn)
+        if keys[K_x]:
+            turn = -delta_turn
+            ship.Turn(turn)
+        if keys[K_SPACE]:    
+            ship.Thrust(thrust)
+            if delta_tau == 0.0:
+                delta_tau = 1.0
+        else:
+            ship.Thrust(0.0)
+        if keys[K_0]:
+            ship.SetGlobalVel3(ship.GetOrientation()*0.0)
+        if keys[K_5]:
+            ship.SetGlobalVel3(ship.GetOrientation()*50.0)
+        if keys[K_9]:
+            ship.SetGlobalVel3(ship.GetOrientation()*90.0)
+        if delta_tau != 0.0:
+            u.MoveToObserverClock(tau)
+            tau += delta_tau 
+        
+    #Draw Everything
+        screen.blit(background, (0, 0))  
+        surf.lock()
+        for obj in objects:
+            obj.Draw(surf, center)
+        surf.unlock()
+        pygame.display.flip()
+
+#Game Over
+
+#this calls the 'main' function when this script is executed
+if __name__ == '__main__': main()
